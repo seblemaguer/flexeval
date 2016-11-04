c8e6833b769f5e03efc51c4c3a55863914a670e0
Alexis <alexis.blond@numericable.fr>
2016-11-04 12:15:29 +0100

    modif bdd quasi fonctionnelle (manque la gestion de id_system et syst_index à l'ajout dans scrript.py)

--- script.py
+++ script.py
@@ -22,18 +22,26 @@ def createDB(JSONfile):
 	dbName = "tests"
 	con = sqlite3.connect(dbName+".db") # Warning: This file is created in the current directory
 	# con.execute("attach DATABASE '"+dbName+".db' as tests")
-	con.execute("CREATE TABLE sample (`sample_id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `type` TEXT NOT NULL, `name` TEXT NOT NULL, `symbole` TEXT NOT NULL DEFAULT 'A', `index_sym` INTEGER NOT NULL, `nb_processed` INTEGER NOT NULL DEFAULT 0)")
-	con.execute("CREATE TABLE answer (`id_answer` INTEGER NOT NULL PRIMARY KEY  AUTOINCREMENT UNIQUE, `type_question` INTEGER NOT NULL, `date` TEXT NOT NULL, `sample1` INTEGER, `sample2` INTEGER, `sample3` INTEGER)")
+	con.execute("CREATE TABLE system (`id` TEXT NOT NULL PRIMARY KEY UNIQUE, `name` TEXT NOT NULL, `comment` TEXT NOT NULL)")
+	con.execute("CREATE TABLE sample (`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `path` TEXT NOT NULL, `type` TEXT NOT NULL, `id_system` INTEGER NOT NULL , `syst_index` INTEGER NOT NULL, `nb_processed` INTEGER NOT NULL DEFAULT 0)")
+	con.execute("CREATE TABLE answer (`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `user` TEXT NOT NULL, `date` TEXT NOT NULL, `content` TEXT NOT NULL, `syst_index` INTEGER NOT NULL, `question_index` INTEGER NOT NULL)")
+
 	con.commit()
 
 	print("------------")
 	print(" DB filling ")
 	print("------------")
 
-	# for item in data["test"]["systems"]["system"]:
-	# 	pprint(item)
-	# 	print("\n")
-	# 	con.execute("INSERT INTO sample VALUES ("+item[0]+","+item[1]+","+item[2]+","+item[3]+","+item[4]+")")
+	for system in data["test"]["systems"]["system"]:
+		systemToInsert=(system["-id"],system["-name"],system["comment"])
+		con.execute("INSERT INTO system(id, name, comment) VALUES (?,?,?)", systemToInsert)
+		for samples in system["samples"]:
+			sampleType=samples["-type"]
+			for samplePath in samples["sample"]:
+				sampleToInsert=(samplePath, sampleType, 0, 0)
+				con.execute("INSERT INTO sample(path, type, id_system, syst_index) VALUES (?,?,?,?)", sampleToInsert)
+	con.commit()
+	con.close()
 
 
 
@@ -43,17 +51,16 @@ createDB("test.json")
 # print json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
 
 # def getTableDump(db_file, table_to_dump):
-#     conn = sqlite3.connect(':memory:')
-#     cursor = conn.cursor()
-#     cursor.execute("attach database '" + db_file + "' as attached_db")
-#     cursor.execute("select sql from attached_db.sqlite_master where type='table' and name='" + table_to_dump + "'")
-#     sql_create_table = cursor.fetchone()[0]
-#     cursor.execute(sql_create_table);
-#     cursor.execute("insert into " + table_to_dump +
-#                " select * from attached_db." + table_to_dump)
-#     conn.commit()
-#     cursor.execute("detach database attached_db")
-#     return "\n".join(conn.iterdump())
+# 	conn = sqlite3.connect(':memory:')
+# 	cursor = conn.cursor()
+# 	cursor.execute("attach database '" + db_file + "' as attached_db")
+# 	cursor.execute("select sql from attached_db.sqlite_master where type='table' and name='" + table_to_dump + "'")
+# 	sql_create_table = cursor.fetchone()[0]
+# 	cursor.execute(sql_create_table);
+# 	cursor.execute("insert into " + table_to_dump + " select * from attached_db." + table_to_dump)
+# 	conn.commit()
+# 	cursor.execute("detach database attached_db")
+# 	return "\n".join(conn.iterdump())
 
 # TABLE_TO_DUMP = 'table_to_dump'
 # DB_FILE = 'db_file'
