6eb0ad847b952b550c4a0834c78f760f32f24da1
Alexis <alexis.blond@numericable.fr>
2017-01-13 11:01:59 +0100

    create table with new columns from csv (describe in json)

--- generator.py
+++ generator.py
@@ -6,6 +6,7 @@ import shutil
 import sqlite3
 import sys
 import argparse
+
 from pprint import pprint
 
 execfile(os.path.join(os.path.dirname(__file__),'pm_bodies.py'))
@@ -39,7 +40,7 @@ def parse_arguments():
 			else:
 				lsName.append(elt)
 		if len(lsName) != len(lsPath):
-			sys.exit('Abort: Bad number of arguments (in systems argument)')
+			sys.exit('ABORT: Bad number of arguments (in systems argument)')
 	else:
 		lsPath = args.systems
 
@@ -53,7 +54,7 @@ def create_architecture(testName):
 	def create_dir(path, name):
 		dir = str(path)+str(name)+'/'
 		if os.path.exists(dir):
-			sys.exit('Abort: Folder already exist : '+dir)
+			sys.exit('ABORT: Folder already exist : '+dir)
 		os.makedirs(dir)
 		print(dir+' created.')
 		return dir
@@ -82,6 +83,22 @@ def parse_json(JSONfile):
 	print('Done.\n')
 	return data
 
+def load_csv(lsPath, lsName):
+	print('|----------|')
+	print('| load CSV |')
+	print('v----------v')
+
+	for csvPath in lsPath:
+		if not os.path.isfile(csvPath):
+			sys.exit('ABORT: '+csvPath+' must be a file')
+		with open(csvPath, 'rb') as csvfile:
+			spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+			if verbose:
+				for row in spamreader:
+					print ', '.join(row)
+
+	# TODO: return csv_data + csv_headers
+
 def create_db(data):
 	print('|---------------|')
 	print('| DB generation |')
@@ -89,23 +106,23 @@ def create_db(data):
 
 	con = sqlite3.connect(testDirectory+'/data.db')
 	try:
-		systs = "`system1` TEXT NOT NULL"
+		systs = '`system1` TEXT NOT NULL'
 		for i in range(1,nbSystemToDisplay):
-			systs = systs + ", `system"+str(i+1) + "` TEXT NOT NULL"
-		con.execute("CREATE TABLE system (`id` TEXT NOT NULL PRIMARY KEY UNIQUE, `name` TEXT NOT NULL, `comment` TEXT NOT NULL)")
-		con.execute("CREATE TABLE sample (`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `path` TEXT NOT NULL, `type` TEXT NOT NULL, `id_system` TEXT NOT NULL , `syst_index` INTEGER NOT NULL, `nb_processed` INTEGER NOT NULL DEFAULT 0, FOREIGN KEY(id_system) REFERENCES system(id))")
-		con.execute("CREATE TABLE answer (`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `user` TEXT NOT NULL, `date` TEXT NOT NULL, `content` TEXT NOT NULL, `content_target` TEXT, `syst_index` INTEGER NOT NULL, `question_index` INTEGER NOT NULL,"+systs+" )")
+			systs = systs + ', `system'+str(i+1) + '` TEXT NOT NULL'
+		con.execute('CREATE TABLE system (`id` TEXT NOT NULL PRIMARY KEY UNIQUE, `name` TEXT NOT NULL, `comment` TEXT NOT NULL)')
+		con.execute('CREATE TABLE sample (`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `path` TEXT NOT NULL, `type` TEXT NOT NULL, `id_system` TEXT NOT NULL , `syst_index` INTEGER NOT NULL, `nb_processed` INTEGER NOT NULL DEFAULT 0, FOREIGN KEY(id_system) REFERENCES system(id))')
+		con.execute('CREATE TABLE answer (`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `user` TEXT NOT NULL, `date` TEXT NOT NULL, `content` TEXT NOT NULL, `content_target` TEXT, `syst_index` INTEGER NOT NULL, `question_index` INTEGER NOT NULL,'+systs+' )')
 		con.commit()
 		print('Database successfully created.')
 		for system in data['systems']['system']:
 			systemIndex = 0
 			systemToInsert = (system['id'],system['name'],system['comment'])
-			con.execute("INSERT INTO system(id, name, comment) VALUES (?,?,?)", systemToInsert)
+			con.execute('INSERT INTO system(id, name, comment) VALUES (?,?,?)', systemToInsert)
 			for samples in system['samples']:
 				sampleType = samples['type']
 				for samplePath in samples['sample']:
 					sampleToInsert=(samplePath, sampleType, system['id'], systemIndex)
-					con.execute("INSERT INTO sample(path, type, id_system, syst_index) VALUES (?,?,?,?)", sampleToInsert)
+					con.execute('INSERT INTO sample(path, type, id_system, syst_index) VALUES (?,?,?,?)', sampleToInsert)
 					systemIndex += 1
 		con.commit()
 		print('Database successfully filled.')
@@ -127,7 +144,7 @@ def generate_config(json):
 	if 'configuration' in configJson:
 		configJson = configJson['configuration']
 	else:
-		sys.exit('Abort: Invalid JSON file')
+		sys.exit('ABORT: Invalid JSON file')
 
 	print('Configuration JSON:')
 	print(configJson)
@@ -162,15 +179,15 @@ def generate_config(json):
 			config.write(expected+'=\'unknow\'\n')
 		if expected == 'nbQuestions':
 			print('ERROR :: '+expected)
-			sys.exit('Abort: Invalid JSON file')
+			sys.exit('ABORT: Invalid JSON file')
 		if expected == 'nbSteps':
 			print('ERROR :: '+expected)
-			sys.exit('Abort: Invalid JSON file')
+			sys.exit('ABORT: Invalid JSON file')
 		if expected == 'nbIntroductionSteps':
 			config.write(expected+'=\'0\'\n')
 		if expected == 'nbSystemDisplayed':
 			print('ERROR :: '+expected)
-			sys.exit('Abort: Invalid JSON file')
+			sys.exit('ABORT: Invalid JSON file')
 		if expected == 'description':
 			config.write(expected+'=\'\'\n')
 		if expected == 'welcomeText':
@@ -317,6 +334,7 @@ dataFromJSON = parse_json(inputJSON)
 name = dataFromJSON['configuration']['name']
 (mainDirectory, testDirectory, viewsDirectory, staticDirectory, mediaDirectory) = create_architecture(name)
 generate_config(dataFromJSON)
+load_csv(lsPath, lsName)
 create_db(dataFromJSON)
 # generate_template() # TODO: not used, move to verif_template
 copy_templates(inputTemplate, indexTemplate, completedTemplate)
