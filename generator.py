fcd02684d8fcf4b4185bf5ac977763c9a173b1ad
Alexis <alexis.blond@numericable.fr>
2017-01-20 16:59:07 +0100

    nettoyage 3...

--- generator.py
+++ generator.py
@@ -3,21 +3,22 @@ import argparse
 import csv
 import json
 import os
+import random
 import re
 import shutil
 import sqlite3
+import string
 import sys
 from pprint import pprint
-import random
-import string
 
-execfile(os.path.join(os.path.dirname(__file__),'pm_bodies.py'))
-global prefix
-prefix=''
-global tok
-tok=''
+execfile(os.path.join(os.path.dirname(__file__), 'pm_bodies.py'))
+# global prefix
+# prefix=''
+# global tok
+# tok=''
 global warning
-warning=[]
+warning = []
+
 
 def parse_arguments():
 	print('	╔════════════════╗')
@@ -25,8 +26,8 @@ def parse_arguments():
 	print('	╚════════════════╝')
 	print('')
 
-	JSONFile = None
-	TemplateFile = None
+	# JSONFile = None
+	# TemplateFile = None
 	parser = argparse.ArgumentParser(description='Generator for subjective test web platform')
 	parser.add_argument('-j', '--json', help='input JSON file', type=argparse.FileType('r'), required=True)
 	parser.add_argument('-t', '--main-tpl', help='input main template file', type=str, required=True)
@@ -39,25 +40,27 @@ def parse_arguments():
 	parser.add_argument('--csv-delimiter', help='define csv delimiter (default: ;)', default=';')
 	args = parser.parse_args()
 
-	global verbose 
+	global verbose
 	verbose = args.verbose
 	if verbose:
 		print('verbose mode enabled')
+
 	global csv_delimiter
+	csv_delimiter = ';'
 	if len(args.csv_delimiter) == 1 or args.csv_delimiter == 'tab':
 		csv_delimiter = args.csv_delimiter
 	elif len(args.csv_delimiter) == 3 and args.csv_delimiter[0] == '\'' and args.csv_delimiter[2] == '\'':
 		csv_delimiter = args.csv_delimiter[1]
 	else:
-		print('Warning: bad csv delimiter. The default delimiter is used.')
+		addWarning('bad csv delimiter. The default delimiter (;) is used.')
 	if verbose:
-		print('csv_delimiter is: '+csv_delimiter)
-	
+		print('csv_delimiter is: ' + csv_delimiter)
+
 	lsPath = []
 	lsName = []
-	if(args.name):
+	if args.name:
 		for index, elt in enumerate(args.systems):
-			if index%2==0:
+			if index % 2 == 0:
 				lsPath.append(elt)
 			else:
 				lsName.append(elt)
@@ -68,6 +71,7 @@ def parse_arguments():
 
 	return args.json, lsPath, lsName, args.main_tpl, args.index_tpl, args.completed_tpl, args.export_tpl
 
+
 def create_architecture(testName):
 	if verbose:
 		print('|-----------------------|')
@@ -75,25 +79,25 @@ def create_architecture(testName):
 		print('v-----------------------v')
 
 	def create_dir(path, name):
-		std_dir = str(path)+str(name)
+		std_dir = str(path) + str(name)
 		dir = std_dir
 		i = 0
 		while os.path.exists(dir):
 			if verbose:
-				print('Folder '+dir+' already exist.')
-			i+=1
-			dir = std_dir+'_'+str(i)
-		if i>0:
-			addWarning('Folder '+std_dir+' aready exist.')
-			addWarning('New folder at '+dir)
+				print('Folder ' + dir + ' already exist.')
+			i += 1
+			dir = std_dir + '_' + str(i)
+		if i > 0:
+			addWarning('Folder ' + std_dir + ' aready exist.')
+			addWarning('New folder at ' + dir)
 		os.makedirs(dir)
 		if verbose:
-			print(dir+' created.')
-		return dir+'/'
+			print(dir + ' created.')
+		return dir + '/'
 
-	simpleTestName = re.sub('\W','_', testName)
+	simpleTestName = re.sub('\W', '_', testName)
 
-	mainDirectory = os.path.join(os.path.dirname(__file__),'tests/')
+	mainDirectory = os.path.join(os.path.dirname(__file__), 'tests/')
 	if not os.path.exists(mainDirectory):
 		os.makedirs(mainDirectory)
 
@@ -105,6 +109,7 @@ def create_architecture(testName):
 	if verbose:
 		print('Done.\n')
 	return mainDirectory, testDirectory, viewsDirectory, staticDirectory, mediaDirectory
+
 
 def load_json(JSONfile):
 	if verbose:
@@ -120,6 +125,7 @@ def load_json(JSONfile):
 		print('Done.\n')
 	return data
 
+
 def load_csv(lsPath):
 	if verbose:
 		print('|-------------|')
@@ -129,7 +135,7 @@ def load_csv(lsPath):
 	lsCSV = []
 	for csvPath in lsPath:
 		if not os.path.isfile(csvPath):
-			sys.exit('ABORT: '+csvPath+' must be a file')
+			sys.exit('ABORT: ' + csvPath + ' must be a file')
 		with open(csvPath, 'rb') as csvfile:
 			if csv_delimiter == 'tab':
 				spamreader = csv.reader(csvfile, delimiter='\t', quotechar='"')
@@ -147,25 +153,26 @@ def load_csv(lsPath):
 		print(lsCSV)
 	return lsCSV
 
+
 def create_db(config, data, lsName):
 	if verbose:
 		print('|---------------|')
 		print('| DB generation |')
 		print('v---------------v')
 
-	con = sqlite3.connect(testDirectory+'/data.db')
+	con = sqlite3.connect(testDirectory + '/data.db')
 	headersCSV = config['configuration']['headersCSV']
 	if not lsName:
 		if verbose:
 			print('No system names defined. Default system names will be created:')
 		for index, system in enumerate(data):
-			lsName.append('system_'+str(index))
+			lsName.append('system_' + str(index))
 			if verbose:
-				print('system_'+str(index))
+				print('system_' + str(index))
 	try:
 		systs = '`system1` TEXT NOT NULL'
-		for i in range(1,nbSystemToDisplay):
-			systs = systs + ', `system'+str(i+1) + '` TEXT NOT NULL'
+		for i in range(1, nbSystemToDisplay):
+			systs = systs + ', `system' + str(i + 1) + '` TEXT NOT NULL'
 		columns = ''
 		for header in headersCSV:
 			columns += '`'+header+'` TEXT NOT NULL,'
@@ -186,10 +193,10 @@ def create_db(config, data, lsName):
 					sampleTuple = ()
 					for j in sample:
 						sampleTuple += (j,)
-					sampleTuple += (sampleType, index+1, i,)
+					sampleTuple += (sampleType, index + 1, i,)
 					con.execute('INSERT INTO sample('+', '.join(headersCSV)+', type, id_system, syst_index) VALUES ('+'?,'*len(headersCSV)+'?,?,?)', sampleTuple)
 			con.commit()
-			if verbose :
+			if verbose:
 				print('Database successfully filled.')
 		except Exception as e:
 			print('Exception in filling')
@@ -204,11 +211,13 @@ def create_db(config, data, lsName):
 	if verbose:
 		print('Done.\n')
 
+
 def generate_config(json, lsPath):
 	if verbose:
 		print('|-------------------|')
 		print('| config generation |')
 		print('v-------------------v')
+
 	global nbSystemToDisplay
 	global useMedia
 	global prefix
@@ -218,72 +227,73 @@ def generate_config(json, lsPath):
 		configJson = configJson['configuration']
 	else:
 		sys.exit('ABORT: Invalid JSON file')
-	if verbose :
+	if verbose:
 		print('Configuration JSON:')
 		print(configJson)
 
 	expectedConfig = ['name', 'author', 'nbSteps', 'nbIntroductionSteps', 'nbSystemDisplayed', 'description', 'useMedia', 'nbQuestions', 'nbFixedPosition']
 
-	config = open(testDirectory+'/config.py', 'w')
+	config = open(testDirectory + '/config.py', 'w')
 	config.write('# === CONFIGURATION VARIABLES ===\n')
 	config.write('# Each configuration variable is necessarily a string\n')
 	nbsbs = 0
 	with open(lsPath[0], 'rb') as csvfile:
 		spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
 		nbsbs = sum(1 for row in spamreader)
-	exception=[]
+	exception = []
 	for var in configJson:
 		if var in expectedConfig:
-			if verbose :
-				print(var+'\t:: OK')
+			if verbose:
+				print(var + '\t:: OK')
 			expectedConfig.remove(var)
-		if var=='nbSystemDisplayed':
+		if var == 'nbSystemDisplayed':
 			nbSystemToDisplay = int(configJson[var])
-		elif var=="prefixe":
+		elif var == "prefixe":
 			prefix = configJson[var]
 		elif var == 'useMedia':
 			useMedia = configJson[var]
-			config.write(var+'='+str(configJson[var])+'\n')
+			config.write(var + '=' + str(configJson[var]) + '\n')
 			exception.append(var)
-		elif var == 'headersCSV' :
-			config.write(var+'='+str(configJson[var])+'\n')
+		elif var == 'headersCSV':
+			config.write(var + '=' + str(configJson[var]) + '\n')
 			exception.append(var)
-		elif var == 'nbFixedPosition' :
-			if configJson[var] < 0 or configJson[var] > configJson['nbSteps'] :
-				configJson[var]=nbsbs
+		elif var == 'nbFixedPosition':
+			if configJson[var] < 0 or configJson[var] > configJson['nbSteps']:
+				configJson[var] = nbsbs
 		if var not in exception:
 			if type(configJson[var]) == unicode:
-				config.write((var+'=\''+configJson[var]+'\'\n').encode('UTF-8'))
+				config.write((var + '=\'' + configJson[var] + '\'\n').encode('UTF-8'))
 			else:
-				config.write(var+'=\''+str(configJson[var])+'\'\n')
+				config.write(var + '=\'' + str(configJson[var]) + '\'\n')
 	tok = generate_token()
-	config.write('token=\''+tok+'\'\n')
+	config.write('token=\'' + tok + '\'\n')
 	for expected in expectedConfig:
-		print(expected+' not found!')
+		print(expected + ' not found!')
 		if expected == 'name':
-			config.write(expected+'=\'TEST\'\n')
+			config.write(expected + '=\'TEST\'\n')
 		if expected == 'author':
-			config.write(expected+'=\'unknow\'\n')
+			config.write(expected + '=\'unknow\'\n')
 		if expected == 'nbQuestions':
-			print('ERROR :: '+expected)
+			print('ERROR :: ' + expected)
 			sys.exit('ABORT: Invalid JSON file')
 		if expected == 'nbSteps':
-			print('ERROR :: '+expected)
+			print('ERROR :: ' + expected)
 			sys.exit('ABORT: Invalid JSON file')
 		if expected == 'nbIntroductionSteps':
-			config.write(expected+'=\'0\'\n')
+			config.write(expected + '=\'0\'\n')
 		if expected == 'nbSystemDisplayed':
-			print('ERROR :: '+expected)
+			print('ERROR :: ' + expected)
 			sys.exit('ABORT: Invalid JSON file')
 		if expected == 'description':
-			config.write(expected+'=\'\'\n')
+			config.write(expected + '=\'\'\n')
 		if expected == 'useMedia':
-			config.write(expected+'=[]\n')
-		if expected == 'nbFixedPosition' :
-			config.write(expected+'=\'0\'\n')
-	config.write('nbSampleBySystem=\''+str(nbsbs)+'\'\n')
+			config.write(expected + '=[]\n')
+		if expected == 'nbFixedPosition':
+			config.write(expected + '=\'0\'\n')
+	config.write('nbSampleBySystem=\'' + str(nbsbs) + '\'\n')
 	if verbose:
 		print('Done.\n')
+
 
 def generate_template():
 	if verbose:
@@ -292,7 +302,7 @@ def generate_template():
 		print('v---------------------v')
 
 	shutil.copy(inputTemplate, viewsDirectory)
-	print('template correctly moved to '+viewsDirectory)
+	print('template correctly moved to ' + viewsDirectory)
 
 	print('TEMPLATE VERIFICATION:')
 
@@ -304,9 +314,9 @@ def generate_template():
 	scriptArray = []
 
 	search = re.search(regexName, inputTemplate)
-	if search :
-		tplPath = viewsDirectory+search.group(0)
-	print('template at "'+tplPath+'":')
+	if search:
+		tplPath = viewsDirectory + search.group(0)
+	print('template at "' + tplPath + '":')
 
 	tpl = open(tplPath, 'r').read()
 	for finditer in re.finditer(regexLink, tpl):
@@ -318,27 +328,32 @@ def generate_template():
 	if verbose:
 		print('Done.\n')
 
+
 def create_platform():
 	if verbose:
 		print('|-------------------|')
 		print('| platform creation |')
 		print('v-------------------v')
-	fo = open(testDirectory+'platform.py', 'wb')
+
+	fo = open(testDirectory + 'platform.py', 'wb')
 	fo.write(platform_body)
 	fo.close()
 	if verbose:
 		print('Done.\n')
+
 
 def create_model():
 	if verbose:
 		print('|----------------|')
 		print('| model creation |')
 		print('v----------------v')
-	fo = open(testDirectory+'model.py', 'wb')
+
+	fo = open(testDirectory + 'model.py', 'wb')
 	fo.write(model_body)
 	fo.close()
 	if verbose:
 		print('Done.\n')
+
 
 def copy_media(csv, mediaColumns, config):
 	if verbose:
@@ -346,69 +361,70 @@ def copy_media(csv, mediaColumns, config):
 		print('| media file copy |')
 		print('v-----------------v')
 
-	audio = []
+	media = []
 	audioFolders = []
 	systems = []
 	regex = '^.*\/'
 	systems = csv
 	for system in csv:
 		for sample in system:
-			for col_index,col_content in enumerate(sample):
+			for col_index, col_content in enumerate(sample):
 				if configJSON['configuration']['headersCSV'][col_index] in mediaColumns:
-					audio.append(col_content)
-	for wav in audio:
+					media.append(col_content)
+	for wav in media:
 		search = re.search(regex, wav)
-		if search :
+		if search:
 			if search.group(0) not in audioFolders:
 				audioFolders.append(search.group(0))
 	for folder in audioFolders:
-		os.makedirs(mediaDirectory+folder)
-	for file in audio:
+		os.makedirs(mediaDirectory + folder)
+	for file in media:
 		filedir = ''
 		search = re.search(regex, file)
-		if search :
+		if search:
 			filedir = search.group(0)
-		shutil.copy(file, mediaDirectory+filedir)
+		shutil.copy(file, mediaDirectory + filedir)
 		if verbose:
-			print(file+'  to  '+mediaDirectory+filedir)
+			print(file + '  to  ' + mediaDirectory + filedir)
 	if verbose:
 		print('Done.\n')
+
 
 def verif_template():
 	if verbose:
 		print('|----------------|')
 		print('| template check |')
 		print('v----------------v')
-	authorized_tags=["{{name}}","{{author}}","{{description}}","{{index}}","{{user}}"]
-	warning_tags=["samples","systems"]
-	essentials_tags=["bootstrap.min.css","jquery.js","jquery-ui.min.js","jquery-ui.min.css","addEventListener"]
+	authorized_tags = ["{{name}}", "{{author}}", "{{description}}", "{{index}}", "{{user}}"]
+	warning_tags = ["samples", "systems"]
+	essentials_tags = ["bootstrap.min.css", "jquery.js", "jquery-ui.min.js", "jquery-ui.min.css", "addEventListener"]
 	regexp = '{{[A-z,0-9]+}}'
 	textfile = open(inputTemplate, 'r')
 	filetext = textfile.read()
 	textfile.close()
-	checked=[]
-	miss=0
-	for et in essentials_tags :
+	checked = []
+	miss = 0
+	for et in essentials_tags:
 		matches = re.findall(et, filetext)
-		if len(matches)==0:
-			print('ERRROR \t:: '+ et + ' not found! Please add it our you will get trouble...')
-			miss=miss+1
-	if miss==0 :
+		if len(matches) == 0:
+			print('ERRROR \t:: ' + et + ' not found! Please add it our you will get trouble...')
+			miss += 1
+	if miss == 0:
 		print('Static Files \t:: OK')
-	miss=0
+	miss = 0
 	for t in warning_tags:
-		for i in range(nbSystemToDisplay) :
-			matches = re.findall("{{"+t+"\[["+str(i)+"]+\]}}", filetext)
-			checked.append("{{"+t+"["+str(i)+"]}}")
-			if len(matches)==0:
-				print(t + ' \t:: ERROR - missing '+t+'['+str(i)+'] tag in your template!')
-	if miss==0 :
+		for i in range(nbSystemToDisplay):
+			matches = re.findall("{{" + t + "\[[" + str(i) + "]+\]}}", filetext)
+			checked.append("{{" + t + "[" + str(i) + "]}}")
+			if len(matches) == 0:
+				print(t + ' \t:: ERROR - missing ' + t + '[' + str(i) + '] tag in your template!')
+	if miss == 0:
 		print('Dynamics tags \t:: OK')
 	matches = re.findall(regexp, filetext)
-	for m in matches :
-		if m in checked :
+	for m in matches:
+		if m in checked:
 			continue
-		if not m in authorized_tags :
+		if not m in authorized_tags:
 			print(m + ' \t:: WARN : maybe you have an error in your template, please check if your application works fine !')
 		else :
 			print(m + ' \t:: OK')
@@ -416,25 +432,28 @@ def verif_template():
 	if verbose:
 		print('Done.\n')
 
+
 def copy_templates(inputTemplate, indexTemplate, completedTemplate, exportTemplate):
 	if verbose:
 		print('|----------------|')
 		print('| templates copy |')
 		print('v----------------v')
-	shutil.copy(indexTemplate, viewsDirectory+'index.tpl')
-	shutil.copy(inputTemplate, viewsDirectory+'template.tpl')
-	shutil.copy(completedTemplate, viewsDirectory+'completed.tpl')
-	shutil.copy(exportTemplate, viewsDirectory+'export.tpl')
-	
+	shutil.copy(indexTemplate, viewsDirectory + 'index.tpl')
+	shutil.copy(inputTemplate, viewsDirectory + 'template.tpl')
+	shutil.copy(completedTemplate, viewsDirectory + 'completed.tpl')
+	shutil.copy(exportTemplate, viewsDirectory + 'export.tpl')
+
 	if verbose:
 		print('Done.\n')
+
 
 def addWarning(warningMessage):
 	warning.append(warningMessage)
 
+
 def printWarning():
 	for i in warning:
-		print('WARNING: '+i)
+		print('WARNING: ' + i)
 
 
 def generate_token():
@@ -452,17 +471,16 @@ create_platform()
 create_model()
 if useMedia:
 	copy_media(listDataCSV, useMedia, configJSON)
-url=''
-if prefix!='' : 
-	url='server_url/'+prefix+'/export'
-else :
-	url='server_url/export'
+url = ''
+if prefix != '':
+	url = 'server_url/' + prefix + '/export'
+else:
+	url = 'server_url/export'
 
-
-print('You can access the database from the following url : ' + url)
-print('You will need a token for this, so keep this one !!')
-print('Token = '+tok)
+print('You can access the database at the following url : ' + url)
+print('You will need a token for that, so keep this one !!')
+print('Token = ' + tok)
 printWarning()
-print('='*30)
-print('    GENERATION TERMINEE !!')
-print('='*30)
+print('=' * 30)
+print('	GENERATION TERMINEE !!')
+print('=' * 30)
