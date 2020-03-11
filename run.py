158bf1827db2662ed83881fc5d12e10297c90504
cedricfayet <contact@cfayet.eu>
2020-03-11 22:56:33 +0100

    Chgt config
    le fichier de config d'une instance ne contient plus les infos: nom db + host & port.
    
    Le nom de db est devenu generique
    
    Le host et port est determiné par celui qui run le test et non celui qui le conçoit. Dnc ces infos sont récup via arg.

--- run.py
+++ run.py
@@ -13,7 +13,10 @@ if __name__ == '__main__':
     # Arguments Parsing
     parser = argparse.ArgumentParser(description='PercEval')
     parser.add_argument("config",metavar='CONFIG_REP', type=str, help='configuration')
+    parser.add_argument("host",metavar='HOST', type=str, help='127.0.0.1')
+    parser.add_argument("port",metavar='PORT', type=int, help='8080')
     args = parser.parse_args()
+
 
     utils.NAME_REP_CONFIG = os.path.dirname(os.path.abspath(__file__))+"./instances/"+args.config
 
@@ -21,25 +24,25 @@ if __name__ == '__main__':
     with open(utils.NAME_REP_CONFIG+'/config.json') as config:
         utils.config = json.load(config)
 
-    utils.app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+utils.NAME_REP_CONFIG+"/"+utils.config["SQLALCHEMY_DATABASE_URI"]
+    utils.app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+utils.NAME_REP_CONFIG+"/bdd_sqlite"
     utils.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     utils.app.secret_key = b'_5#y2zcer88L"Fczerzce4Q8sdqsdcezqtz\n\xec]/'
     utils.db = SQLAlchemy(utils.app)
 
-    if "auth" in utils.config["use"]:
+    if "auth" in utils.config:
         import mods.auth as ml
         from mods.auth.model import *
         utils.app.register_blueprint(ml.bp,url_prefix='/auth') # Register Blueprint
 
-    if "pages" in utils.config["use"]:
+    if "pages" in utils.config:
         import mods.pages as mp
         utils.app.register_blueprint(mp.bp,url_prefix='/pages') # Register Blueprint
 
-    if "questionnaire" in utils.config["use"]:
+    if "questionnaire" in utils.config:
         import mods.questionnaire as mq
         utils.app.register_blueprint(mq.bp,url_prefix='/questionnaire') # Register Blueprint
 
-    if "tests" in utils.config["use"]:
+    if "tests" in utils.config:
         import mods.tests as mt
         utils.app.register_blueprint(mt.bp,url_prefix='/tests') # Register Blueprint
 
@@ -51,4 +54,4 @@ if __name__ == '__main__':
         return redirect(utils.config["entrypoint"])
 
     # Run app
-    utils.app.run(host=utils.config["HOST"],port=int(utils.config["PORT"]))
+    utils.app.run(host=args.host,port=args.port)
