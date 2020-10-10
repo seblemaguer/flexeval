0405307bc620c146fd80d968d5db15b0a16d41bf
Sebastien Le Maguer <lemagues@tcd.ie>
2020-10-10 12:37:38 +0100

    [app] clean: formatting

--- flexeval/__init__.py
+++ flexeval/__init__.py
@@ -1,63 +1,67 @@
 # coding: utf8
 # license : CeCILL-C
 
+# Python
 import os
 import random
 import string
 import datetime
 
+# Flask
 from flask import Flask, current_app, g
 
+# SQL
 import sqlalchemy
 
-from .utils import safe_copy_rep,safe_make_rep, del_file, create_file
+# FlexEval
+from .utils import safe_make_rep, del_file, create_file
 from .core import Config, Provider, ErrorHandler
 from .core.providers import assets, templates
+from .extensions import db, session_manager
 
-from .extensions import db,session_manager
 
-def create_app(INSTANCE_PATH,INSTANCE_URL):
+def create_app(INSTANCE_PATH, INSTANCE_URL):
     """Application-factory pattern"""
 
-    app = Flask(__name__,template_folder = None,static_url_path=None)
+    app = Flask(__name__, template_folder=None, static_url_path=None)
 
     # Config FLEXEVAL
-    app.config.setdefault("FLEXEVAL_DIR",os.path.dirname(os.path.abspath(__file__)))
-    app.config.setdefault("FLEXEVAL_INSTANCE_DIR",INSTANCE_PATH)
-    app.config.setdefault("FLEXEVAL_INSTANCE_URL",INSTANCE_URL)
-    app.config.setdefault("FLEXEVAL_INSTANCE_TMP_DIR",safe_make_rep(INSTANCE_PATH+"/.tmp"))
+    app.config.setdefault("FLEXEVAL_DIR", os.path.dirname(os.path.abspath(__file__)))
+    app.config.setdefault("FLEXEVAL_INSTANCE_DIR", INSTANCE_PATH)
+    app.config.setdefault("FLEXEVAL_INSTANCE_URL", INSTANCE_URL)
+    app.config.setdefault("FLEXEVAL_INSTANCE_TMP_DIR", safe_make_rep(INSTANCE_PATH+"/.tmp"))
 
     # Config Session
-    app.config.setdefault("SESSION_TYPE","filesystem")
-    app.config.setdefault('PERMANENT_SESSION_LIFETIME',datetime.timedelta(days=31))
-    app.config.setdefault('SECRET_KEY',''.join((random.choice(string.ascii_lowercase) for i in range(20))).encode('ascii'))
-    app.config.setdefault('SESSION_FILE_DIR',safe_make_rep(INSTANCE_PATH+"/.tmp/.sessions"))
+    app.config.setdefault("SESSION_TYPE", "filesystem")
+    app.config.setdefault('PERMANENT_SESSION_LIFETIME', datetime.timedelta(days=31))
+    app.config.setdefault('SECRET_KEY', ''.join((random.choice(string.ascii_lowercase) for i in range(20))).encode('ascii'))
+    app.config.setdefault('SESSION_FILE_DIR', safe_make_rep(INSTANCE_PATH+"/.tmp/.sessions"))
 
     # Config SqlAlchemy
-    app.config.setdefault('SQLALCHEMY_FILE',INSTANCE_PATH+"/flexeval.db")
-    app.config.setdefault('SQLALCHEMY_DATABASE_URI',"sqlite:///"+app.config["SQLALCHEMY_FILE"])
-    app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS',False)
+    app.config.setdefault('SQLALCHEMY_FILE', INSTANCE_PATH+"/flexeval.db")
+    app.config.setdefault('SQLALCHEMY_DATABASE_URI', "sqlite:///"+app.config["SQLALCHEMY_FILE"])
+    app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', False)
 
-    # On intialise la co à la db
+    # Initialisation of the DB connection
     db.init_app(app)
 
-    # On initialise session manager
+    # Session manager initialisation
     session_manager.init_app(app)
 
     # Init
     with app.app_context():
 
-        # On instancie les providers par défault
+        # Instantiating the default providers
         assets.default("/assets")
-        templates.default(app.config["FLEXEVAL_INSTANCE_TMP_DIR"]+"/templates")
+        templates.default(app.config["FLEXEVAL_INSTANCE_TMP_DIR"] + "/templates")
 
         # Config app based on structure.json
         Config()
 
-        # Tous les modules ont été chargés donc on peut gen la db
+        # Module loaded => create the database
         db.create_all()
 
-        # Gestion des erreurs
+        # Error management
         ErrorHandler()
 
     return app
