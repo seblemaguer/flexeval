cbc1edd916afe5cf723fbed6356e96fc8a037484
Sebastien Le Maguer <lemagues@tcd.ie>
2020-10-20 22:51:27 +0100

    [provider] clean: format TemplateProvider

--- flexeval/core/providers/assets/AssetsProvider.py
+++ flexeval/core/providers/assets/AssetsProvider.py
@@ -1,34 +1,42 @@
 # coding: utf8
 # license : CeCILL-C
 
-from flask import abort,current_app,send_from_directory
+from flask import abort, current_app, send_from_directory
 
 from flexeval.core import ProviderFactory
+
 
 class AssetsProviderError(Exception):
     pass
 
-class UnknowSourceError(AssetsProviderError):
 
-    def __init__(self,path,_from):
+class UnknowSourceError(AssetsProviderError):
+    def __init__(self, path, _from):
         self.path = path
         self._from = _from
+
 
 def decode(path):
     try:
         repositories = path.split("/")
-        file = repositories[len(repositories)-1]
-        repositories = repositories[:len(repositories)-1]
+        file = repositories[len(repositories) - 1]
+        repositories = repositories[: len(repositories) - 1]
 
-        assert not(file == "." or file == ".." or file == "~" or file == "__pycache__")
+        assert not (file == "." or file == ".." or file == "~" or file == "__pycache__")
 
         for repository in repositories:
-            assert not(repository == "." or repository == ".." or repository == "~" or repository == "__pycache__")
+            assert not (
+                repository == "."
+                or repository == ".."
+                or repository == "~"
+                or repository == "__pycache__"
+            )
 
-        return repositories,file
+        return repositories, file
 
     except Exception as e:
         abort(403)
+
 
 def generate_path(repositories):
 
@@ -39,52 +47,76 @@ def generate_path(repositories):
     return path
 
 
-class AssetsProvider():
-
-    def __init__(self,url_prefix):
+class AssetsProvider:
+    def __init__(self, url_prefix):
         self.url_prefix = url_prefix
-        current_app.add_url_rule(self.url_prefix+'/<path:path>',self.__class__.__name__+':assets:'+url_prefix,self.get_content)
-        ProviderFactory().set("assets",self)
-        print(" * AssetsProvider:"+self.__class__.__name__+" loaded and bound to: "+self.url_prefix)
+        current_app.add_url_rule(
+            self.url_prefix + "/<path:path>",
+            self.__class__.__name__ + ":assets:" + url_prefix,
+            self.get_content,
+        )
+        ProviderFactory().set("assets", self)
+        print(
+            " * AssetsProvider:"
+            + self.__class__.__name__
+            + " loaded and bound to: "
+            + self.url_prefix
+        )
 
+    def local_url(self, path, _from=None):
 
-    def local_url(self,path,_from=None):
-
-        if not(path[0] == "/"):
-            path = "/"+path
+        if not (path[0] == "/"):
+            path = "/" + path
 
         if _from is None:
             return self.url_instance(path)
         elif _from == "flexeval":
             return self.url_flexeval(path)
         elif _from[:3] == "mod":
-            return self.url_mod(_from[4:],path)
+            return self.url_mod(_from[4:], path)
         else:
-            raise UnknowSourceError(path,_from)
+            raise UnknowSourceError(path, _from)
 
-    def url_flexeval(self,path):
-        return self.url_prefix+"/flexeval"+path
+    def url_flexeval(self, path):
+        return self.url_prefix + "/flexeval" + path
 
-    def url_mod(self,name_mod,path):
-        return self.url_prefix+"/flexeval/mods/"+name_mod+path
+    def url_mod(self, name_mod, path):
+        return self.url_prefix + "/flexeval/mods/" + name_mod + path
 
-    def url_instance(self,path):
-        return self.url_prefix+path
+    def url_instance(self, path):
+        return self.url_prefix + path
 
-    def get_content(self,path):
+    def get_content(self, path):
 
-        repositories,file = decode(path)
+        repositories, file = decode(path)
 
         try:
             try:
-                return send_from_directory(current_app.config["FLEXEVAL_INSTANCE_DIR"]+"/assets/"+generate_path(repositories),file)
+                return send_from_directory(
+                    current_app.config["FLEXEVAL_INSTANCE_DIR"]
+                    + "/assets/"
+                    + generate_path(repositories),
+                    file,
+                )
             except Exception as e:
                 if repositories[0] == "flexeval":
                     try:
-                        return send_from_directory(current_app.config["FLEXEVAL_DIR"]+"/assets/"+generate_path(repositories[1:]),file)
+                        return send_from_directory(
+                            current_app.config["FLEXEVAL_DIR"]
+                            + "/assets/"
+                            + generate_path(repositories[1:]),
+                            file,
+                        )
                     except Exception as e:
                         if repositories[1] == "mods":
-                            return send_from_directory(current_app.config["FLEXEVAL_DIR"]+"/mods/"+repositories[2]+"/assets/"+generate_path(repositories[3:]),file)
+                            return send_from_directory(
+                                current_app.config["FLEXEVAL_DIR"]
+                                + "/mods/"
+                                + repositories[2]
+                                + "/assets/"
+                                + generate_path(repositories[3:]),
+                                file,
+                            )
                         else:
                             abort(404)
                 else:
