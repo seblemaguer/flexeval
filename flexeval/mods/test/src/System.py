1e6af33c469fae55ca21eee0017072f8b0136f1f
Sebastien Le Maguer <lemagues@tcd.ie>
2020-10-20 10:56:36 +0100

    [mod] clean: format test module

--- flexeval/mods/test/src/System.py
+++ flexeval/mods/test/src/System.py
@@ -5,44 +5,51 @@ from flask import current_app
 from sqlalchemy import asc
 
 from flexeval.utils import AppSingleton
-from flexeval.database import db,commit_all
+from flexeval.database import db, commit_all
 
 from flexeval.mods.test.model import SystemSample
 
-class SystemError(Exception):
 
-    def __init__(self,message):
+class SystemError(Exception):
+    def __init__(self, message):
         self.message = message
+
 
 class SystemFileNotFound(SystemError):
     pass
 
 
 class SystemManager(metaclass=AppSingleton):
-
     def __init__(self):
-        self.register={}
+        self.register = {}
 
-    def get(self,name):
-        if not(name in self.register):
+    def get(self, name):
+        if not (name in self.register):
             self.register[name] = System(name)
 
         return self.register[name]
 
-class System():
 
-    def __init__(self,name):
+class System:
+    def __init__(self, name):
 
         if name[0] == "/":
             name = name[1:]
 
         self.name = name
-        source_file = current_app.config["FLEXEVAL_INSTANCE_DIR"]+"/systems/"+self.name+".csv"
+        source_file = (
+            current_app.config["FLEXEVAL_INSTANCE_DIR"]
+            + "/systems/"
+            + self.name
+            + ".csv"
+        )
 
         try:
-            reader = csv.DictReader(open(source_file,encoding='utf-8'))
+            reader = csv.DictReader(open(source_file, encoding="utf-8"))
         except Exception as e:
-            raise SystemFileNotFound(source_file+" doesn't exist. Fix test.json or add the system's file.")
+            raise SystemFileNotFound(
+                source_file + " doesn't exist. Fix test.json or add the system's file."
+            )
 
         self.cols_name = reader.fieldnames
 
@@ -50,26 +57,35 @@ class System():
         # Il est important de faire cela avant de créer les instances de SystemSample
         # Sinon seulement les columns définies de base dans SystemSample seront disponibles.
         for col_name in self.cols_name:
-            SystemSample.addColumn(col_name,db.String)
+            SystemSample.addColumn(col_name, db.String)
 
         if len(self.system_samples) == 0:
 
             for line_id, line in enumerate(reader):
 
-                vars={"system":self.name,"line_id":line_id}
+                vars = {"system": self.name, "line_id": line_id}
 
                 try:
                     for col_name in self.cols_name:
-                        vars[col_name]=line[col_name]
+                        vars[col_name] = line[col_name]
 
-                    SystemSample.create(commit=False,**vars)
+                    SystemSample.create(commit=False, **vars)
 
                 except Exception as e:
-                    raise SystemError("Issue to read the line "+str(line_id)+" of the file:"+source_file)
+                    raise SystemError(
+                        "Issue to read the line "
+                        + str(line_id)
+                        + " of the file:"
+                        + source_file
+                    )
 
             commit_all()
 
     @property
     def system_samples(self):
 
-        return SystemSample.query.filter(SystemSample.system == self.name).order_by(SystemSample.line_id.asc()).all()
+        return (
+            SystemSample.query.filter(SystemSample.system == self.name)
+            .order_by(SystemSample.line_id.asc())
+            .all()
+        )
