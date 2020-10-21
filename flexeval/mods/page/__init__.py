b25717770fa6dafc2585c6dd6938e3fd1d38d3c4
Sebastien Le Maguer <lemagues@tcd.ie>
2020-10-21 09:04:26 +0100

    [module] clean: format page init

--- flexeval/mods/page/__init__.py
+++ flexeval/mods/page/__init__.py
@@ -4,12 +4,12 @@
 from flask import current_app
 
 from flexeval.core import StageModule
-from flexeval.database import Column,db
+from flexeval.database import Column, db
 from flexeval.utils import redirect
 
-with StageModule(__name__,subname="visitor") as sm:
+with StageModule(__name__, subname="visitor") as sm:
 
-    @sm.route("/", methods = ['GET'])
+    @sm.route("/", methods=["GET"])
     def main():
         stage = sm.current_stage
         authProvider = sm.authProvider
@@ -27,9 +27,10 @@ with StageModule(__name__,subname="visitor") as sm:
         else:
             return sm.render_template(template=stage.template)
 
-with StageModule(__name__,subname="user") as sm_user:
 
-    @sm_user.route("/", methods = ['GET'])
+with StageModule(__name__, subname="user") as sm_user:
+
+    @sm_user.route("/", methods=["GET"])
     @sm_user.connection_required
     def main():
         stage = sm_user.current_stage
