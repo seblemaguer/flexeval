5bd68b1c5950fa48a2e35b039b8d3c123ce3092d
Sebastien Le Maguer <lemagues@tcd.ie>
2020-10-21 09:04:35 +0100

    [core] clean: format core init

--- flexeval/core/__init__.py
+++ flexeval/core/__init__.py
@@ -2,10 +2,10 @@
 # license : CeCILL-C
 
 from .ProviderFactory import ProviderFactory, UndefinedError
-from .src.AuthProvider import AuthProvider,UserBase
+from .src.AuthProvider import AuthProvider, UserBase
 
 from .src.ErrorHandler import ErrorHandler
 from .src.Config import Config
 
-from .src.Stage import StageModule,Stage
+from .src.Stage import StageModule, Stage
 from .src.Admin import AdminModule
