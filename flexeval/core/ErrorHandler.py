b26cd493586fc99a972e13eca87d8881a1ac5f95
Sebastien Le Maguer <lemagues@tcd.ie>
2021-01-25 10:06:00 +0000

    [core] clean: ErrorHandler

--- flexeval/core/ErrorHandler.py
+++ flexeval/core/ErrorHandler.py
@@ -14,38 +14,68 @@ from .Module import Module
 
 
 class ErrorHandler(metaclass=AppSingleton):
-    error_managers = dict()
+    """Class which defines how errors are handled.
+
+    Class Attributes
+    ==========
+    error_alternative_handlers: dict of <Error Class> => function(error: <Error Class>)
+       Dictionnary which provides an alternative handler for a specific error which is of type Error Class
+    """
+
+    error_alternative_handlers = dict()
+
+    @classmethod
+    def add_alternative_handler(cls, error_class, error_alternative_handler):
+        """Add a new handler for a given error class
+
+        Parameters
+        ----------
+        cls: Class
+            the class ErrorHandler
+        error_class: Class
+            The error class
+        error_alternative_handler: function(error: <Error Class>)
+            The alternative handler
+        """
+        ErrorHandler.error_alternative_handlers[error_class] = error_alternative_handler
+
     def __init__(self):
+        """Constructor
+
+        Parameters
+        ----------
+        self: ErrorHandler
+            The current object
+        """
         self._logger = logging.getLogger(self.__class__.__name__)
+
+        # Define the default handler to be the method "error"
         current_app.register_error_handler(Exception, self.error)
 
-    def trace(self, e):
+    def error(self, e):
+        """The error handler routine entry point.
+
+        It is also in charge of executin the alternative handler, if one is defined for the type of error given in parameters.
+
+        Parameters
+        ----------
+        self: ErrorHandler
+            The current object
+        e: Exception
+            The exception to handle
+        """
+
+        # Deal with critical server errors
         code = 500
         if not (isinstance(e, HTTPException)):
-            self._logger.critical("Error \"%s\"" % str(e))
+            self._logger.critical('Error "%s"' % str(e))
             self._logger.critical("Traceback: ")
             for eline in traceback.format_exc().splitlines():
                 self._logger.critical(eline)
         else:
             code = e.code
 
-        return code
-
-    @classmethod
-    def add_error_manager(cls, error_class, error_manager):
-        ErrorHandler.error_managers[error_class] = error_manager
-
-    def error(self, e):
-        error_class = None
-        for cur_error_class in ErrorHandler.error_managers.keys():
-            if isinstance(e, cur_error_class):
-                error_class = cur_error_class
-                break
-
-        if error_class is not None:
-            return ErrorHandler.error_managers[error_class](e)
-
-        code = self.trace(e)
+        # Render the error page
         return Module.render_template(
             ProviderFactory().get("templates").get("/error.tpl"),
             parameters={"code": code},
