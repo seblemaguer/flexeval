e79886da0e0cac1bfe35a15b762f06fb40a5dd16
Sebastien Le Maguer <lemagues@tcd.ie>
2024-02-15 11:45:49 +0200

    [test] fix: duplicate user field

--- flexeval/mods/test/model.py
+++ flexeval/mods/test/model.py
@@ -1,7 +1,7 @@
 # coding: utf8
 from datetime import datetime
 
-from flexeval.database import Model, relationship, Column, ForeignKey, db, declared_attr
+from flexeval.database import Model, Column, ForeignKey, db, declared_attr
 from flexeval.core import StageModule
 
 usermodel = StageModule.get_UserModel()
@@ -28,13 +28,13 @@ class TestModel(Model):
 
     A test is composed by different steps and each row of the table corresponds to one of these steps.
 
-    A step is identified by an \"id\", has a \"date\" when it was created, and index (\"step_idx\") and a flag indicating if it is an introduction step (\"intro\")
+    A step is identified by an \"id\", has a \"date\" when it was created, and index (\"step_idx\") and a flag
+    indicating if it is an introduction step (\"intro\")
     """
 
     __abstract__ = True
 
     id = Column(db.Integer, primary_key=True)
-
     date = db.Column(db.DateTime, nullable=False)
     step_idx = db.Column(db.Integer, nullable=False)
     intro = db.Column(db.Boolean, nullable=False)
@@ -47,7 +47,3 @@ class TestModel(Model):
     @declared_attr
     def user_id(cls):
         return Column(db.String, ForeignKey(usermodel.__tablename__ + ".id"))
-
-    @declared_attr
-    def user(cls):
-        return relationship(usermodel.__name__, primaryjoin=usermodel.__name__ + ".id==%s.user_id" % cls.__name__)
