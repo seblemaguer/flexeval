3b781b5baef8dc21c14c7345de64e7923c1dbf82
Sebastien Le Maguer <lemagues@tcd.ie>
2024-03-03 09:08:40 +0200

    [auth] add: bootstrapped prolific authentication

--- flexeval/mods/direct_auth/model/User.py
+++ flexeval/mods/direct_auth/model/User.py
@@ -2,6 +2,7 @@
 # license : CeCILL-C
 
 from flexeval.core.providers.auth import UserModel
+from flexeval.database import Column, db
 
 import re
 
@@ -22,3 +23,20 @@ class EmailUser(UserModel):
 
         # Set the email as the ID
         self.id = email
+
+
+class ProlificUser(UserModel):
+    study_id = Column(db.String, default="")
+    session_id = Column(db.String, default="")
+
+    def __init__(self, email, study_id, session_id):
+        # Validate email
+        if not re.fullmatch(EMAIL_REGEX, email):
+            raise NotAnEmail(email)
+
+        # Set the email as the ID
+        self.id = email
+
+        # Get other fields
+        self.study_id = study_id
+        self.session_id = session_id
